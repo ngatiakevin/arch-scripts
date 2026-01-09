5a2ded3ee52930fbd9b57fc55f37950e26e73afd
kevo <ngatia.kevin.mail@gmail.com>
2026-01-10 01:06:05 +0300

    Update arch-chroot .sh

--- arch-chroot .sh
+++ arch-chroot .sh
@@ -7,8 +7,10 @@ nano /etc/vconsole.conf
 
 nano /etc/hostname
 nano /etc/hosts
+passwd
 useradd -mG wheel kevo
 passwd kevo
+EDITOR=nano visudo
 
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
